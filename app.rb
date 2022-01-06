601fc6e66d6466aa2fcb23d661e0845d6e690367
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-01-06 09:19:04 +0000

    added a form tag to the view, which sense form data to the url defined by the form's action attribute. Two input fields: the text type input field sends name data back to the server as the name parameter

--- app.rb
+++ app.rb
@@ -22,3 +22,13 @@ get '/cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
 end
 
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample 
+  erb(:index)
+end   
+
+get '/named-cat' do
+  p params 
+  @name = params[:name]
+  erb(:index)
+end  
